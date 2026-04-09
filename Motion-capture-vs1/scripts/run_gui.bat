571d4c6db4bbc8eecc21d4d4fc6f71ffb833d68d
Pedamallu Sai Mrudula <mrudulasankar2007@gmail.com>
2026-04-09 11:26:40 +0530

    Delete Motion-capture-vs1 directory

--- Motion-capture-vs1/scripts/run_gui.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Starting MoCap VS5...
-cd /d "%~dp0.."
-venv\Scripts\python.exe main_gui.py
-pause
--- Motion-capture-vs1/scripts/run_master.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Starting MoCap VS5 MASTER MODE...
-cd /d "%~dp0.."
-set /p REMOTE_IP="Enter remote PC IP (e.g. 10.137.227.217): "
-echo Remote Camera: %REMOTE_IP%
-venv\Scripts\python.exe launch_multi_camera.py --mode master --remote-ip %REMOTE_IP%
-pause
